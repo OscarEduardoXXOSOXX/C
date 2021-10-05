f4c4e40930473ceda8178097b51c015323a7393f
Oscar Eduardo Mendoza Alcantara <91708243+OscarEduardoXXOSOXX@users.noreply.github.com>
2021-10-05 14:43:22 -0500

    Add files via upload
    Hola mundo en C

---
+++ HolaMundo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+
+	printf("\nHello world C\nViva el TEsji\nViva México /,,/\n");
+	printf("Prueba de guardado\n");
+	printf("Segunda Prueba\n");
+
+	return 0;
+
+}
